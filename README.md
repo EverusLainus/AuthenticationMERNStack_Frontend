1c22040dfdbdcd95a5191a18535d48147ab8e17b
Everus <everuslainus@gmail.com>
2025-04-23 14:49:24 +0400

    added profile about and readme pages

--- README.md
+++ README.md
@@ -1,9 +1,58 @@
-## Functionalities
+# Authentication App
 
-- Signup
-- Login
-- Logout
+A modern full-stack authentication application built with **React**, **Node.js**, and **MongoDB**,and deployed on **Netlify** (frontend) and **Render** (backend). Styled with Chakra UI for a clean, responsive UI.
 
-# Features
+---
 
-- responsive UI
+## Features
+
+- User Sign Up
+- Secure Login with JWT
+- Logout functionality
+- Error Handling
+- Responsive design using Chakra UI
+
+---
+
+## Tech Stack
+
+### Frontend
+
+- [React](https://reactjs.org/)
+- [Chakra UI](https://chakra-ui.com/)
+- [Axios](https://axios-http.com/)
+- [Netlify](https://www.netlify.com/) (Deployment)
+
+### Backend
+
+- [Node.js](https://nodejs.org/)
+- [Express.js](https://expressjs.com/)
+- [MongoDB Atlas](https://www.mongodb.com/cloud/atlas)
+- [Mongoose](https://mongoosejs.com/)
+- [JSON Web Tokens (JWT)](https://jwt.io/)
+- [Render](https://render.com/) (Deployment)
+
+---
+
+## Live Links
+
+- **Frontend (Netlify)**: [https://authenticationmernstack-backend.onrender.com](https://authenticationmernstack-backend.onrender.com)
+- **Backend (Render)**: [https://authenticationmernstack-backend.onrender.com](hhttps://authenticationmernstack-backend.onrender.com)
+
+---
+
+## Development Tools Used
+
+- [VS Code](https://code.visualstudio.com/) – Coding and debugging
+- [Insomnia](https://insomnia.rest/) – API testing
+- [MongoDB Compass](https://www.mongodb.com/products/compass) – MongoDB GUI client
+
+## ⚙️ Local Setup Instructions
+
+### 🔐 Frontend
+
+```bash
+git clone https://github.com/EverusLainus/AuthenticationMERNStack_Backend.git
+cd AuthenticationMERNStack_Backend
+npm install
+```
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,81 @@
 import { Flex } from "@chakra-ui/react";
 
-export const Profile = () => {
+("use client");
+
+import {
+  Avatar,
+  Box,
+  Center,
+  Stack,
+  Button,
+  useColorModeValue,
+} from "@chakra-ui/react";
+
+export function Profile() {
   return (
     <Flex minH={"90vh"} align={"center"} justify={"center"} bg={"#FDF6FA"}>
-      <h1>Profile page</h1>
+      <Center py={6}>
+        <Box
+          maxW={"320px"}
+          w={"full"}
+          bg={useColorModeValue("white", "gray.900")}
+          boxShadow={"2xl"}
+          rounded={"lg"}
+          p={6}
+          textAlign={"center"}
+        >
+          <Avatar
+            size={"xl"}
+            src={
+              "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ"
+            }
+            mb={4}
+            pos={"relative"}
+            _after={{
+              content: '""',
+              w: 4,
+              h: 4,
+              bg: "green.300",
+              border: "2px solid white",
+              rounded: "full",
+              pos: "absolute",
+              bottom: 0,
+              right: 3,
+            }}
+          />
+
+          <Stack mt={8} direction={"row"} spacing={4}>
+            <Button
+              flex={1}
+              fontSize={"sm"}
+              rounded={"full"}
+              _focus={{
+                bg: "gray.200",
+              }}
+            >
+              Message
+            </Button>
+            <Button
+              flex={1}
+              fontSize={"sm"}
+              rounded={"full"}
+              bg={"#F361AF"}
+              color={"white"}
+              boxShadow={
+                "0px 1px 25px -5px rgb(66 153 225 / 48%), 0 10px 10px -5px rgb(66 153 225 / 43%)"
+              }
+              _hover={{
+                bg: "#FDAED8",
+              }}
+              _focus={{
+                bg: "#F361AF",
+              }}
+            >
+              Follow
+            </Button>
+          </Stack>
+        </Box>
+      </Center>
     </Flex>
   );
-};
+}
